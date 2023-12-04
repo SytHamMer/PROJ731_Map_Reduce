9bebaa5f52c73ea079cb689e0d40a781a64e1dac
SytHamMer <mathys.lebon1@gmail.com>
2023-12-04 16:54:19 +0100

    [ADD]: repartitor method (need to be test)

--- Repartitor.java
+++ Repartitor.java
@@ -8,4 +8,24 @@ public class Repartitor {
         this.mapResult = mapResult;
         this.nbReducer = nbReducer;
     }
+
+
+    // NEED TO BE TEST !
+    public HashMap<Integer,ArrayList<HashMap>> repartitor(){
+        //res Dict with id of reducer in key and list of dict of it in value
+
+        HashMap<Integer,ArrayList<HashMap>> res = new HashMap<>();
+        //initialisation of this dic
+        for(int i =0;i<nbReducer;i++) {
+            res.put(i, new ArrayList<>());
+
+        }
+        for(int j=0; j<mapResult.size();j++){
+
+            res.put(j,mapResult.get(j));
+
+        }
+
+        return res;
+    }
 }
